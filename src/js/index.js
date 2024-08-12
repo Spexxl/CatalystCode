269e98ac156873bcccc5d63955cbb941e45fb9e4
Spexxl <snexxlorioh@outlook.com>
2024-08-11 21:33:38 -0300

    Primeiro Commit

---
+++ src/js/index.js
@@ -0,0 +1,79 @@
+window.revelar = ScrollReveal({reset:true})
+
+
+//Home
+revelar.reveal('.Home-left',{
+    duration: 1200,
+    distance: '15%'
+});
+revelar.reveal('.Home-banner',{
+    duration: 1200,
+    distance: '15%',
+});
+revelar.reveal('.Home-bottom-bar',{
+    duration: 1200,
+    distance: '15%',
+    delay: 400,
+    origin: "left",
+});
+revelar.reveal('.Beneficios-home',{
+    duration: 1200,
+    distance: '15%',
+    delay: 300,
+    origin: "left",
+});
+
+
+//Sobre nos
+revelar.reveal('.sobre-nos',{
+    duration: 1000,
+    distance: '15%',
+});
+revelar.reveal('.sobre-nos-content',{
+    duration: 1000,
+    distance: '15%',
+    origin: "left",
+    delay: 250,
+});
+revelar.reveal('.sobre-nos-banner',{
+    duration: 1000,
+    distance: '15%',
+    origin: "right",
+    delay: 300,
+});
+
+
+//Nos Escolher
+revelar.reveal('.porque-nos-escolher',{
+    duration: 1000,
+    distance: '15%',
+});
+revelar.reveal('.SN-box',{
+    duration: 1000,
+    distance: '15%',
+    delay: 200,
+});
+
+
+//Contato
+revelar.reveal('.Contact-us',{
+    duration: 1000,
+    distance: '15%',
+});
+revelar.reveal('.Contact-us-form',{
+    duration: 1000,
+    distance: '15%',
+    delay: 200,
+});
+
+
+//Serviços
+revelar.reveal('.Serviços',{
+    duration: 1000,
+    distance: '15%',
+});
+revelar.reveal('.Serviços-box',{
+    duration: 1000,
+    distance: '15%',
+    delay: 200,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'cinza-escuro': '#141414',
+      },
+      fontFamily: {
+        sans: ["Roboto", 'sans-serif'],
+        display: ["Roboto Serif", 'serif'],
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
